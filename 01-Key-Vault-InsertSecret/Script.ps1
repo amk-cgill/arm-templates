3626f3936ee15facf3866e85f5ec840a91090ca5
Craig Gill <craig.gill@assetmark.com>
2020-08-29 00:43:38 -0700

    3rd

--- 01-Key-Vault-InsertSecret/Script.ps1
+++ 01-Key-Vault-InsertSecret/Script.ps1
@@ -1,11 +1,12 @@
-$resourceGroupName = "Read-Host -Prompt "Enter the name of the Resource Group that contains the Key Vault 
-$keyVaultName = Read-Host -Prompt "Enter the name of the KeyVault to copy the secret to"
-$secretName = Read-Host -Prompt "Enter the service account name"
+
+
+$resourceGroupName = 'azncvs01webapprg1' #Read-Host -Prompt "Enter the name of the Resource Group that contains the Key Vault"
+$keyVaultName = 'azncvs01webappkv1' #Read-Host -Prompt "Enter the name of the KeyVault to copy the secret to"
+$secretName = 'criag.gill' #Read-Host -Prompt "Enter the service account name"
 $secretValue = Read-Host -Prompt "Enter the service account password" -AsSecureString
 
-$adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
+$upn = $(Get-AzContext).Account.Id
+$azuser = $(Get-AzADUser -UserPrincipalName $upn).Id
 $templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -secretValue $secretValue
-
-Write-Host "Press [ENTER] to continue ..."
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -adUserId $azuser -keyVaultName $keyVaultName -secretValue $secretValue -Verbose
