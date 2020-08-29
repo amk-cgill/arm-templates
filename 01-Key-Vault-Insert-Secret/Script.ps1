e19835c98e3dd414f2806b87a3bff63b1e90b1f6
Craig Gill <craig.gill@assetmark.com>
2020-08-29 15:56:50 -0700

    Remove dependsOn parameter

--- 01-Key-Vault-Insert-Secret/Script.ps1
+++ 01-Key-Vault-Insert-Secret/Script.ps1
@@ -1,8 +1,8 @@
 $resourceGroupName = 'azncvs01webapprg1' #Read-Host -Prompt "Enter the name of the Resource Group that contains the Key Vault"
 $keyVaultName = 'azncvs01webappkv1' #Read-Host -Prompt "Enter the name of the KeyVault to copy the secret to"
-$secretName = Read-Host -Prompt "Enter the service account name"
+$secretName = Read-Host -Prompt "Enter the secret name"
 $secretValue = Read-Host -Prompt "Enter the service account password" -AsSecureString
 
-$templateUri = "https://raw.githubusercontent.com/amk-cgill/arm-templates/master/01-Key-Vault-InsertSecret/azuredeploy.json"
+$templateUri = "https://raw.githubusercontent.com/amk-cgill/arm-templates/master/01-Key-Vault-Insert-Secret/azuredeploy.json"
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -secretValue $secretValue -Verbose
