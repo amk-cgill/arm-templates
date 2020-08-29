8994e31431aeb2d3522b4627d76c32c75864b6ee
Craig Gill <craig.gill@assetmark.com>
2020-08-29 15:26:09 -0700

    changes

--- 01-Key-Vault-Insert-Secret/Script.ps1
+++ 01-Key-Vault-Insert-Secret/Script.ps1
@@ -3,8 +3,6 @@ $keyVaultName = 'azncvs01webappkv1' #Read-Host -Prompt "Enter the name of the Ke
 $secretName = Read-Host -Prompt "Enter the service account name"
 $secretValue = Read-Host -Prompt "Enter the service account password" -AsSecureString
 
-$upn = $(Get-AzContext).Account.Id)
-$azuser = $(Get-AzADUser -UserPrincipalName $upn).Id
 $templateUri = "https://raw.githubusercontent.com/amk-cgill/arm-templates/master/01-Key-Vault-InsertSecret/azuredeploy.json"
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -adUserId $azuser -keyVaultName $keyVaultName -secretValue $secretValue -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -secretValue $secretValue -Verbose
